dcdc6002315a2b2d41fa62820952ef3780d357af
Simon Brundin <54498413+simonbrundin@users.noreply.github.com>
2023-10-21 17:45:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-echo 'Nu kör vi1'
+echo '0.2'
 # Gå till mapp med repos
 set PATH_TO_REPOS_FOLDER '~/Repos'
 cd 
 cd $PATH_TO_REPOS_FOLDER
-pwd
+
 # Välj projektnamn
 read -p 'Projektnamn: ' PROJECT_NAME
 echo $PROJECT_NAME
+
+read PROJECT_NAMN
+echo $PROJECT_NAMN
 
 # Klona new-project
 #git clone https://github.com/simonbrundin/new-project.git $PROJECT_NAME
