ef5987b65d5c42bdab7d094bd04f951fab820c5e
Simon Brundin <simonbrundin@gmail.com>
2023-10-21 23:17:16 +0200

    skapar nu ett nytt github repo också

--- start.sh
+++ start.sh
@@ -1,25 +1,29 @@
+# Välj projektnamn
+read -p 'Projektnamn: ' PROJECT_NAME
+echo $PROJECT_NAME
+
 # Gå till mapp med repos
 PATH_TO_REPOS_FOLDER='Repos'
 cd
 cd $PATH_TO_REPOS_FOLDER
 
-# Välj projektnamn
-read -p 'Projektnamn: ' PROJECT_NAME
-echo $PROJECT_NAME
-
 # Klona new-project
 git clone https://github.com/simonbrundin/new-project.git $PROJECT_NAME
+cd $PROJECT_NAME
+git remote remove origin
+
+# Radera start.sh
+rm start.sh
+
+# Skapa GitHub repository
+gh auth login
+gh repo create --source . --push --private
 
 # Installera npm paket
-NUXT_PATH=$PROJECT_NAME'/frontend/nuxt'
+NUXT_PATH=frontend/nuxt
 cd $NUXT_PATH
 bun install
 
-# Radera start.sh
-cd
-cd $PATH_TO_REPOS_FOLDER
-cd $PROJECT_NAME
-rm start.sh
-
 # Öppna i Visual Studio Code
+cd && cd $PATH_TO_REPOS_FOLDER && cd $PROJECT_NAME
 code .
